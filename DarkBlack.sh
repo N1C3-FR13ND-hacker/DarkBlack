a94d96e7ab06192d51e05847557c5417c9c0521b
N1C3-FR13ND-hacker <66993314+N1C3-FR13ND-hacker@users.noreply.github.com>
2020-06-16 14:59:12 +0700

    Add files via upload

---
+++ DarkBlack.sh
@@ -0,0 +1,670 @@
+#import mashral
+#gak usah di recode=ðŸ˜‰ðŸ˜ƒðŸ˜†ðŸ˜ƒðŸ˜¯ðŸ˜ ðŸ˜¯ðŸ˜¦ðŸ˜²ðŸ˜°ðŸ˜¦ðŸ˜§ðŸ˜�ðŸ™…ðŸ˜šðŸ˜ŒðŸ˜˜ðŸ˜ŒðŸ˜�ðŸ˜·ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜�ðŸ˜®ðŸ˜�ðŸ˜°ðŸ˜³ðŸ˜©ðŸ˜§ðŸ˜²ðŸ˜§ðŸ˜§ðŸ˜§ðŸ˜²ðŸ˜§ðŸ˜²ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜°ðŸ˜³ðŸ˜§ðŸ˜§ðŸ˜©ðŸ˜§ðŸ˜³ðŸ˜©ðŸ˜°ðŸ˜�ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜©ðŸ˜³ðŸ˜›ðŸ˜©ðŸ˜³ðŸ˜©ðŸ˜©ðŸ˜§ðŸ˜�ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜²ðŸ˜ŸðŸ˜§ðŸ˜£ðŸ˜¢ðŸ˜¦ðŸ˜²ðŸ˜£ðŸ˜‚ðŸ˜ƒðŸ˜¯ðŸ˜‰ðŸ˜‚ðŸ˜†ðŸ˜ƒðŸ˜ƒðŸ˜¯ðŸ˜¯ðŸ˜¯ðŸ˜‘ðŸ˜‘ðŸ˜¡ðŸ˜•ðŸ˜¢ðŸ˜…ðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜¯ðŸ˜¯ðŸ˜„ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜�ðŸ˜ ðŸ˜¦ðŸ˜•ðŸ˜¦ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜¢ðŸ˜†ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜¯ðŸ˜†ðŸ˜¡ðŸ˜†ðŸ˜¢ðŸ˜ƒðŸ˜†ðŸ˜¢ðŸ˜•ðŸ˜¯ðŸ˜•ðŸ˜¯ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜†ðŸ˜¢ðŸ˜†ðŸ˜¢ðŸ˜¢ðŸ˜•ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜†ðŸ˜¢ðŸ˜¯ðŸ˜…ðŸ˜†ðŸ˜¢ðŸ˜¢ðŸ˜•ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜•ðŸ˜†ðŸ˜¢ðŸ˜¯ðŸ˜†ðŸ˜¢ðŸ˜†ðŸ˜†ðŸ˜•ðŸ˜•ðŸ˜¡ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ƒðŸ˜ƒðŸ˜€ðŸ˜¯ðŸ˜…ðŸ˜…ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜‘ðŸ˜¡ðŸ˜‘ðŸ˜‘ðŸ˜¯ðŸ˜‘ðŸ˜¯ðŸ˜¡ðŸ˜•ðŸ˜•ðŸ˜¯ðŸ˜¯ðŸ˜‘ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜•ðŸ˜¯ðŸ˜…ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜¢ðŸ˜‘ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜¢ðŸ˜†ðŸ˜¡ðŸ˜¯ðŸ˜¡ðŸ˜†ðŸ˜¡ðŸ˜†ðŸ˜¡ðŸ˜•ðŸ˜¡ðŸ˜• teks:Ð´Ð²Ã‡Î´Ð·fÐ±Ð½ijÐºlÐ¼Ð¸Ã¸Ñ€qÑ�sÑ‚Ñ†vÑ‰xzzÐ´Ð²Ã‡Î´Ð·fÐ±Ð½ijÐºlÐ¼Ð¸Ã¸Ñ€qÑ�sÑ‚Ñ†vÑ‰xzz aÐ¬cdÑ”fgÐ½Ñ—jÐºlÐ¼Ð¸Ð¾pqÑ�$Ñ‚Ñ†vÑˆÐ¶zzaÐ¬cdÑ”fgÐ½Ñ—jÐºlÐ¼Ð¸Ð¾pqÑ�$Ñ‚Ñ†vÑˆÐ¶zz Î±Ð²Â¢Ä‘Ñ”Æ’gÐ½Î¹ÄµÐºÄ»Ð¼Î·ÏƒÏ�qÑ�Ñ•Ñ‚Ï…Î½Ï‰Ï‡zzÎ±Ð²Â¢Ä‘Ñ”Æ’gÐ½Î¹ÄµÐºÄ»Ð¼Î·ÏƒÏ�qÑ�Ñ•Ñ‚Ï…Î½Ï‰Ï‡zz Î±ÃŸÂ¢â…¾â„¯Æ’â�¡â„Œá¸¯× ÐºÅ‚áƒ¦Î·Ã¸â„˜â“ Ñ�ï¹©Ñ‚ÊŠâ™¥áº˜âœ–â„¨â„¨Î±ÃŸÂ¢â…¾â„¯Æ’â�¡â„Œá¸¯× ÐºÅ‚áƒ¦Î·Ã¸â„˜â“ Ñ�ï¹©Ñ‚ÊŠâ™¥áº˜âœ–â„¨â„¨ Î¬Ð²Â¢Ä‘Ñ”à¤¿Î´Ð‚Ä¯ÄµÐºâ„“Ð¼Ð¹Ã°Ï�qÑ�Å¡Ñ‚Ï‹Î½ÏŽÏ‡Å¾Å¾Î¬Ð²Â¢Ä‘Ñ”à¤¿Î´Ð‚Ä¯ÄµÐºâ„“Ð¼Ð¹Ã°Ï�qÑ�Å¡Ñ‚Ï‹Î½ÏŽÏ‡Å¾Å¾ Ã¤ÃŸÃ§Ã�Ø¹FgÑ’Â¡Ê–kâ„“Ð¼â‚ªÙ¥ÃžQÑ�Â§Ñ‚ÂµâˆšÐ¨xÅ¾Å¾Ã¤ÃŸÃ§Ã�Ø¹FgÑ’Â¡Ê–kâ„“Ð¼â‚ªÙ¥ÃžQÑ�Â§Ñ‚ÂµâˆšÐ¨xÅ¾Å¾ Î±ÐªÏ‚Î´Î­fÐ±Ð‹Î�jÐºÄ»Ð¼Ð¸ÏƒpqÑ�ÅŸÏ„Ï…vÏŽÏ‡Î±ÐªÏ‚Î´Î­fÐ±Ð‹Î�jÐºÄ»Ð¼Ð¸ÏƒpqÑ�ÅŸÏ„Ï…vÏŽÏ‡ Î±Î²cÎ´ÎµÅ¦Ä�hÎ¹jÎºlÊ�Ï€Ã¸Ï�Ï†Æ¦$â€ uÏ…Ï‰Ï‡zzÎ±Î²cÎ´ÎµÅ¦Ä�hÎ¹jÎºlÊ�Ï€Ã¸Ï�Ï†Æ¦$â€ uÏ…Ï‰Ï‡zz Î›ÃŸÆ‡DÆ�FÆ“Ä¤ÄªÄ´Ò Ä¿MÐ˜â™¡á¹–Ò¨Å”SÅ¦Æ±âˆšÑ Ó¾ZZÎ›ÃŸÆ‡DÆ�FÆ“Ä¤ÄªÄ´Ò Ä¿MÐ˜â™¡á¹–Ò¨Å”SÅ¦Æ±âˆšÑ Ó¾ZZ ï¾‘ä¹ƒÎ¶Ã�ä¹‡ï½·Ç¤ã‚“ï¾‰ï¾Œã‚ºï¾šá„Šåˆ€Ô¾ï½±Qå°ºä¸‚ï½²Ð¦Ð�Ð©ï¾’ä¹™ä¹™ï¾‘ä¹ƒÎ¶Ã�ä¹‡ï½·Ç¤ã‚“ï¾‰ï¾Œã‚ºï¾šá„Šåˆ€Ô¾ï½±Qå°ºä¸‚ï½²Ð¦Ð�Ð©ï¾’ä¹™/
+
+
+
+
+
+#gak usah di edit edit nanti bisa eror script nya
+#facebook name=Andi zet
+#github name=N1C3-FR13ND
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#gak usah di recode ya
+#yang recode besok mama nya mati
+#bapak kau
+#gak tau malu recode script orang
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo
+figlet "selamat"
+sleep 2
+figlet "datang"
+sleep 2
+figlet "di"
+sleep 2
+figlet "script"
+sleep 2
+figlet "N1C3"
+sleep 2
+figlet "FR13ND"
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo $red "Sebelum Manggunakan Script ini ada harus"
+echo $red "Daftar terlebih dahulu"
+echo $red "Agar tidak terkena Chackpoint"
+echo $cyan "Di Link ini->http://zaonguyen.us/94502//CoPf0/?sc=1&sc>"
+echo $red "Anda copy Link nya lalu paste ke browser anda"
+sleep 20
+clear
+pkg install lolcat
+pkg install ruby
+pkg install figlet
+pkg install toilet
+sleep 1
+clear
+toilet -f standard "DARK" -F gay
+figlet "Facebook" | lolcat
+echo 
+echo "[+===========================================+]" | lolcat
+echo "|By : N1C3 FR13ND                             |" | lolcat
+echo "|Facebook : Andi Zet (Hacker pro)             |" | lolcat
+echo "|No WA : 089524437115                         |" | lolcat
+echo "[+===========================================+]" | lolcat
+echo
+echo
+sleep 2
+echo $blue "+>Masukan ID anda<+"
+read -p " [ ID ]> " insta;
+if [ $insta = ID ] || [ $insta = ID ]
+then
+clear
+echo $yellow "Mencari ID Anda"
+sleep 5
+echo
+echo
+echo
+echo $red  "ID Benar"
+sleep 3
+echo
+fi
+
+
+
+clear
+echo $blue "+>Masukan Password anda<+"
+read -p " [ Pass ]> " insta;
+if [ $insta = Pass ] || [ $insta = Pass ]
+then
+clear
+echo $cyan "Mencari Password anda"
+echo
+echo
+echo
+sleep 5
+echo
+echo $red "Password Lu B€ñãR"
+sleep 3
+echo
+fi
+
+
+clear
+echo $green  "█████████"
+echo $green  "█▄█████▄█"
+echo $green  "█▼▼▼▼▼   __   __"
+echo $green  "█    __       __    HACKER N1C3 FR13ND" 
+echo $green  "█▲▲▲▲▲   __       __"
+echo $green  "█████████"
+echo $green  " ██ ██"
+echo
+echo
+echo $cyan   "{+----------------------------------------+}"
+echo $cyan   "|+BY : N1C3 FR13ND                         |"
+echo $cyan   "|+Name Fb : Andi Zet                       |"
+echo $cyan   "|+NO Wa : 089524437115                     |"
+echo $cyan   "{+----------------------------------------+}"
+echo
+echo
+echo $red    "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+echo $cyan   "[10][HACK FACEBOOK]"
+echo $red    "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+echo
+echo $cy"┌==="$bi"[ N1C3 FR13ND ]"
+echo $cy"¦"
+read -p"-->" root;
+
+
+
+if [ $root = 10 ] || [ $root = 1 ]
+then
+clear
+echo $red   "+>------------------------------------<+"
+sleep 3
+echo $cyan  "+>Mencari Daftar teman                <+"
+sleep 5
+echo $cyan  "+>Menerobos sistem keamanan           <+"
+sleep 6
+echo $cyan  "+>Mencari Email Temen                 <+"
+sleep 7
+echo $cyan  "+>Mencari Password temen              <+"
+sleep 3
+echo $red   "+>------------------------------------<+"
+sleep 5
+clear
+echo $blue  "Sukses\/"
+sleep 3
+echo
+clear
+fi
+
+
+
+echo  $blue  "Email>angganata@gmail.com" 
+echo  $blue  "Password>sayangqu21 vk"
+echo
+sleep 4
+echo  $blue  "email>nata_vantovehl@yahoo.com" 
+echo  $blue  "Password>sayangqu21 fb"
+echo
+sleep 3
+echo  $blue  "Email>tetinurhayati17@yahoo.com" 
+echo  $blue  "Password>18021987"
+echo
+sleep 2
+echo  $blue  "Email>thedjo_ thedjo_bbc@yahoo.coid" 
+echo  $blue  "Password>bangsat fs"
+echo
+sleep 2
+echo  $blue  "Email>den_ghi2@yahoo.com" 
+echo  $blue  "Password>703878 fb"
+echo
+sleep 4
+echo  $blue  "Email>firman_11fauzan@yahoo.co.id" 
+echo  $blue  "pasaword>021186"
+echo
+sleep 3
+echo  $blue  "Email>2@yahoo.com" 
+echo  $blue  "Password>703878"
+echo
+sleep 3
+echo  $blue  "Email>inoonkbau@yahoo.co m" 
+echo  $blue  "Password>udahlupa"
+echo
+echo
+sleep 5
+echo  $blue  "Email>popotikol@yahoo.com" 
+echo  $blue  "Password>bangsat"
+echo
+sleep 4
+echo  $blue  "Email>limited-edition45@yahoo.com"
+echo  $blue  "password>4p44jj4h"
+echo
+sleep 4
+echo  $blue  "Email>yinmungil@yahoo.com" 
+echo  $blue  "Password>050987"
+echo
+sleep 4
+echo  $blue  "Email>wila_caem@yahoo.com" 
+echo  $blue  "Password>290887"
+echo
+sleep 4
+echo  $blue  "Email>ta_rahman88@yahoo.co.id" 
+echo  $blue  "Password>14012008"
+echo
+sleep 4
+echo  $blue  "Email>dedeheru@ymail.com" 
+echo  $blue  "Password>222222"
+echo
+sleep 3
+echo  $blue  "Email>hariken_x@yahoo.co.id" 
+echo  $blue  "Passworda.rohmat"
+echo
+sleep 5
+echo  $blue  "Email>paja_10@yahoo.com" 
+echo  $blue  "Password>05081993"
+echo
+sleep 4
+echo  $blue  "Email>hary.jahari@yahoo.com" 
+echo  $blue  "Password>tawruz"
+echo
+sleep 4
+echo  $blue  "Email>fazryabizz@yahoo.com" 
+echo  $blue  "Password>susunona"
+echo
+sleep 4
+echo  $blue  "Email>h.kasep@yahoo.co.id" 
+echo  $blue  "Password>270390"
+echo
+sleep 4
+echo  $blue  "Email>pratama_agy@yahoo.co.id" 
+echo  $blue  "Password>19061992"
+echo
+sleep 3
+echo  $blue  "Email>ajay_bebh@yahoo.co.id" 
+echo  $blue  "Password>avenged"
+echo
+sleep 4
+echo  $blue  "Email>syoulhiya@yahoo.com" 
+echo  $blue  "Password>08071991"
+echo
+sleep 3
+echo  $blue  "Email>chiya_cardcaptor16@yahoo.com" 
+echo  $blue  "Password>sakurachi"
+echo
+sleep 4
+echo  $blue  "Email>ntik.sditya@gmail.com" 
+echo  $blue  "Password>manggadua fs"
+echo
+sleep 5
+echo  $blue  "Email>sary_hanny@yahoo.co.id" 
+echo  $blue  "Password>109443 ym"
+echo
+sleep 4
+echo  $blue  "Email>imuethsary@yahoo.com" 
+echo  $blue  "Password>109443 fb"
+echo
+sleep 3
+echo  $blue  "Email>be_fu45@yahoo.co.id "
+echo  $blue  "Password180407"
+echo
+echo
+echo $yellow  "Selesai/|\"
+echo $cyan    "Nikmat"
+sleep 20
+clear
